4e87cbfac7919b51c3ad33c9c0c6ebb3648d695b
Simon Pedersen <90264457+orskoven@users.noreply.github.com>
2021-11-01 17:24:19 +0100

    only last assignment left

--- src/ExerciseSix.java
+++ src/ExerciseSix.java
@@ -1,2 +1,24 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class ExerciseSix {
+    static ArrayList<Integer> numbers = new ArrayList<Integer>();
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        for (int i = 1; i < 101; i++) {
+            numbers.add(i);
+        }
+        System.out.println(numbers);
+        System.out.println("Please enter which number multiply to remove:");
+        int numberToRemove = scanner.nextInt();
+        for (int i  = 0; i < numbers.size(); i++) {
+           if (numbers.get(i) % numberToRemove == 0 && numberToRemove != numbers.get(i)){
+               numbers.remove(i);
+           }
+        }
+        System.out.println(numbers);
+    }
 }
+
+
