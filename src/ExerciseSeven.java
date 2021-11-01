9b0e2d9a9863126ee07601f4845767aa68070a5b
Simon Pedersen <90264457+orskoven@users.noreply.github.com>
2021-11-01 19:31:19 +0100

    the last touch has been layed hopefully

--- src/ExerciseSeven.java
+++ src/ExerciseSeven.java
@@ -1,2 +1,17 @@
 public class ExerciseSeven {
+<<<<<<< HEAD
+=======
+    /*
+    Static variables and methods can be used without instantiating the class.
+    This can be used when you don't want to instantiate a class and the value
+    are the same withing the methode or variables.
+
+    See under EXERCISE THREE FOR EXMAPLES and beneath
+     */
+
+    public static void main(String[] args) {
+        System.out.println("This is the number of saved games: ");
+        ExerciseThree.getSavedGame();
+    }
+>>>>>>> 780ccfb (the last touch has been layed hopefully)
 }
